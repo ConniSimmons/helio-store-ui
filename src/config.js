820b20e533bc4a8bcf15faa9335d2c1606efdddb
Conni Simmons <connisimmons@gmail.com>
2020-08-02 18:46:53 -0600

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "helio-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://vj108c71gl.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_qAWZUOFI3",
+      APP_CLIENT_ID: "2c5vihmb1aopk9hra92h8b0qa",
+      IDENTITY_POOL_ID: "us-east-2:f7f41c2e-10f0-4e55-93a4-b13b5f084df7"
+    }
+  };
